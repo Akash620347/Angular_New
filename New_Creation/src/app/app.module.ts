39dbe299a3de62f0b08eb1ebc1bb2969d10978b5
Akash620347 <akash983530@gmail.com>
2024-03-09 23:01:12 +0530

    "Ui Chnages"

--- New_Creation/src/app/app.module.ts
+++ New_Creation/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { Game2048Component } from './game-2048/game-2048.component';
 import { HeadingComponent } from './heading/heading.component';
 import { ButtonsComponent } from './buttons/buttons.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Game2048Component,
     HeadingComponent,
-    ButtonsComponent
+    ButtonsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
