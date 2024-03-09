6e1e14219c1bcb5494dd47976babcd1b4ed0e068
Akash620347 <akash983530@gmail.com>
2024-03-09 19:07:33 +0530

    .

--- New_Creation/src/app/app.module.ts
+++ New_Creation/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Game2048Component } from './game-2048/game-2048.component';
+import { HeadingComponent } from './heading/heading.component';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Game2048Component
+    Game2048Component,
+    HeadingComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule,
