41903e1adb4f79f67728fecf75b002a581d7c374
Restart2205 <learner.hp.bhai22@gmail.com>
2024-03-26 01:40:30 +0530

    "."

--- New_Creation/src/app/app.module.ts
+++ New_Creation/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { Game2048Component } from './game-2048/game-2048.component';
 import { HeadingComponent } from './heading/heading.component';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +18,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- New_Creation/src/app/game-2048/game-2048.component.ts
+++ New_Creation/src/app/game-2048/game-2048.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { animation, trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-game-2048',
   templateUrl: './game-2048.component.html',
-  styleUrl: './game-2048.component.css'
+  styleUrl: './game-2048.component.css',
+  animations:[
+    trigger('numberAnimation',[
+      transition(':enter',[
+        style({transform:'scale(0.2)'}),
+        animate('600ms ease-out', style({transform:'scale(1)'}))
+      ]),
+      // transition(':leave', [
+      //   animate('300ms ease-out', style({ opacity: 0, transform: 'scale(0.5)' }))
+      // ])
+    ])
+  ]
 })
 export class Game2048Component implements OnInit {
   score: number = 0;
   grid: number[][] = [];
-  constructor() { };
+  constructor() {}
 
   ngOnInit(): void {
     this.initializeBoard();
@@ -21,5 +33,19 @@ export class Game2048Component implements OnInit {
         this.grid[i][j] = [];
       }
     }
+    this.positionOfTwo();
   }
+  // to get the two 2's on the screen at random position
+  position: number[][] = [];
+  positionOfTwo(): void {
+    while (this.position.length < 2) {
+      let row = Math.floor(Math.random() * 4);
+      let column = Math.floor(Math.random() * 4);
+      if(!this.position.some(pos=> pos[0] === row && pos[1] === column )){
+        this.position.push([row, column]);
+      }
+    }
+    this.position.forEach(pos=> this.grid[pos[0]][pos[1]] = 2)
+  }
+  
 }
