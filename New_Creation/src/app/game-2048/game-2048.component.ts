512c5da2519331862340a416b3c532868c9d9d23
Restart2205 <learner.hp.bhai22@gmail.com>
2024-03-26 00:01:22 +0530

    .

--- New_Creation/src/app/game-2048/game-2048.component.ts
+++ New_Creation/src/app/game-2048/game-2048.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game-2048',
   templateUrl: './game-2048.component.html',
   styleUrl: './game-2048.component.css'
 })
-export class Game2048Component {
-  grid:number[][] = [];
-  
+export class Game2048Component implements OnInit {
+  score: number = 0;
+  grid: number[][] = [];
+  constructor() { };
+
+  ngOnInit(): void {
+    this.initializeBoard();
+  }
+  // to show grid
+  initializeBoard(): void {
+    for (let i = 0; i < 4; i++) {
+      this.grid[i] = [];
+      for (let j = 0; j < 4; j++) {
+        this.grid[i][j] = [];
+      }
+    }
+  }
 }
